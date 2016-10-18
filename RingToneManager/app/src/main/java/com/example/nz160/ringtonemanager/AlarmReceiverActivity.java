d4c259c4b94201bd2b12341d94054dc17b348b6e
Tejaswini-netzealous <tejaswini.n@netzealous.com>
2016-10-18 16:43:59 +0530

    ok alarm completed

--- RingToneManager/app/src/main/java/com/example/nz160/ringtonemanager/AlarmReceiverActivity.java
+++ RingToneManager/app/src/main/java/com/example/nz160/ringtonemanager/AlarmReceiverActivity.java
@@ -1,6 +1,9 @@
 package com.example.nz160.ringtonemanager;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -9,6 +12,9 @@ import android.media.MediaPlayer;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.TaskStackBuilder;
+import android.support.v7.app.NotificationCompat;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
@@ -23,8 +29,12 @@ import java.io.IOException;
  */
 public class AlarmReceiverActivity extends BroadcastReceiver {
     private MediaPlayer mMediaPlayer;
-
-
+    private NotificationManager mNotificationManager;
+    private Context context;
+    private int numMessages_SingleLine = 0;
+    private final int notificationID_SingleLine = 111;
+    private final long[] pattern = { 100, 300, 300, 300 };// Vibrate pattern in
+    private Uri alarmSound;
 
     private void playSound(Context context, Uri alert) {
         mMediaPlayer = new MediaPlayer();
@@ -60,10 +70,70 @@ public class AlarmReceiverActivity extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
+        this.context=context;
         Toast.makeText(context, "Congrats!. Your Alarm time has been reached", Toast.LENGTH_LONG).show();
-        Intent i = new Intent();
-        i.setClassName("com.example.nz160.ringtonemanager", "com.example.nz160.ringtonemanager.NotificationActivity");
+       /* Intent i = new Intent();
+        i.setClassName("com.example.nz160.ringtonemanager", "com.example.nz160.ringtonemanager.Alarm");
         i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        context.startActivity(i);
+        context.startActivity(i);*/
+        mNotificationManager = (NotificationManager)context. getSystemService(Context.NOTIFICATION_SERVICE);
+        alarmSound = RingtoneManager
+                .getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        final Dialog dialog = new Dialog(context.getApplicationContext());
+        dialog.getWindow().setType(WindowManager.LayoutParams.TYPE_SYSTEM_ALERT);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.alarm);
+        dialog.getWindow().getAttributes().windowAnimations = R.style.dialog_animation;
+        dialog.show();
+        //playSound(context,getAlarmUri());
+
+        Button cancel=(Button)dialog.findViewById(R.id.cancel_alarm);
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mMediaPlayer.stop();
+            }
+        });
+        displayNotificationSingleLine();
+    }
+    /** Single Line Notifications Methods **/
+    protected void displayNotificationSingleLine() {
+        Log.i("Start", "notification");
+		/* Invoking the default notification service */
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(
+               context);// Setting builder
+        mBuilder.setContentTitle("New Message");// title
+        mBuilder.setContentText("You've received new message from Androhub.");// Message
+        mBuilder.setTicker("New Message Alert!");// Ticker
+        mBuilder.setSmallIcon(R.drawable.alarm);// Small icon
+		/* Increase notification number every time a new notification arrives */
+        mBuilder.setNumber(++numMessages_SingleLine);
+
+        mBuilder.setSound(alarmSound);// set alarm sound
+        mBuilder.setVibrate(pattern);// set vibration
+		/* Creates an explicit intent for an Activity in your app */
+        Intent resultIntent = new Intent(context,
+                NotificationActivity.class);
+
+        resultIntent.putExtra("notificationId", notificationID_SingleLine);// put
+        // notification
+        // id
+        // into
+        // intent
+        resultIntent.putExtra("message", "http://androhub.com");//Your message to show in next activity
+
+        // Task builder to maintain task for pending intent
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+        stackBuilder.addParentStack(NotificationActivity.class);
+		/* Adds the Intent that starts the Activity to the top of the stack */
+        stackBuilder.addNextIntent(resultIntent);
+
+        PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0,
+                PendingIntent.FLAG_UPDATE_CURRENT);// Set flag to update current
+        mBuilder.setContentIntent(resultPendingIntent);// set content intent
+
+		/* notificationID allows you to update the notification later on. */
+        mNotificationManager
+                .notify(notificationID_SingleLine, mBuilder.build());
     }
 }
